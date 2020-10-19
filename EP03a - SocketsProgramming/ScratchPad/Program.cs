cdd5826e38a3f36b95bcc80bb222aaef0bed5bae
Richard Weeks <richardleeweeks@icloud.com>
2020-10-18 21:57:29 -0500

    EP03a Ready

---
+++ EP03a - SocketsProgramming/ScratchPad/Program.cs
@@ -0,0 +1,132 @@
+﻿#nullable enable
+
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using System.Xml.XPath;
+
+namespace ScratchPad
+{
+
+    [AttributeUsage( AttributeTargets.Method, AllowMultiple = false )]
+    public class RouteAttribute : Attribute
+    {
+        public string Path { get; }
+
+        public RouteAttribute( string path ) => Path = path;
+    }
+
+
+    public abstract class MessageDispatcher<TMessageType> where TMessageType : class, new()
+    {
+        public abstract void Register<TParam, TResult>( Func<TParam, Task<TResult>> target );
+        public abstract void Register<TParam>( Func<TParam, Task> target );
+        public abstract Task<TMessageType?> DispatchAsync( TMessageType message );
+
+    }
+
+    public class XDocumentMessageDispatcher : MessageDispatcher<XDocument>
+    {
+        readonly List<(string,Func<XDocument,Task<XDocument?>>)> _handlers = new List<(string, Func<XDocument, Task<XDocument?>>)>();
+
+        public override async Task<XDocument?> DispatchAsync( XDocument message )
+        {
+            foreach(var (route,target) in _handlers)
+            {
+                if( (message.XPathEvaluate(route) as bool?) == true)
+                {
+                    return await target( message );
+                }
+            }
+
+            //No Handler registered
+            return null;
+        }
+
+        public override void Register<TParam, TResult>( Func<TParam, Task<TResult>> target )
+        {
+            async Task<XDocument?> wrapper( XDocument xml )
+            {
+                var @param = XmlSerialization.Deserialize<TParam>(xml);
+                var result = await target(@param);
+                if ( result != null )
+                    return XmlSerialization.Serialize( result );
+                else
+                    return null;
+            }
+
+            _handlers.Add( (GetRouteExpression( target.Method ), wrapper) );
+        }
+
+        public override void Register<TParam>( Func<TParam, Task> target )
+        {
+            async Task<XDocument?> wrapper( XDocument xml )
+            {
+                var @param = XmlSerialization.Deserialize<TParam>(xml);
+                await target( @param );
+                return null;
+            }
+
+            _handlers.Add( (GetRouteExpression( target.Method ), wrapper) );
+        }
+
+        string GetRouteExpression( MethodInfo mi )
+        {
+            var route =   mi.GetCustomAttribute<RouteAttribute>( );
+            if ( route == null )
+                throw new ArgumentException( $"{mi.Name} missing RouteAttribute" );
+
+            return $"boolean({route.Path})";
+        }
+    }
+
+    class Program
+    {
+        static async Task Main( string[ ] args )
+        {
+            var messageHandler = new XDocumentMessageDispatcher( ); 
+
+            //register handlers
+            messageHandler.Register<HeartBeatRequestMessage, HeartBeatResponseMessage>( HeartBeatRequestHandler );
+            messageHandler.Register<HeartBeatResponseMessage>( HeartBeatResponseHandler );
+
+            //create hb message and serialize to XDocument
+            var hbRequest = new HeartBeatRequestMessage {
+                Id = "HB_0001",
+                POSData = new POSData { Id = "POS_001"}
+            };
+            var hbRequestXDoc = XmlSerialization.Serialize(hbRequest);
+
+
+            //dispatch the message
+            var responseXDoc = await messageHandler.DispatchAsync(hbRequestXDoc);
+            if ( responseXDoc != null )
+                await messageHandler.DispatchAsync( responseXDoc );
+
+        }
+
+
+        //Handler on the 'Server' side of the system
+        [Route( "/Message[@type='Request' and @action='HeartBeat']" )]
+        public static Task<HeartBeatResponseMessage> HeartBeatRequestHandler( HeartBeatRequestMessage request )
+        {
+            var response = new HeartBeatResponseMessage {
+                Id = request.Id,
+                POSData = request.POSData,
+                Result = new Result { Status = Status.Success}
+            };
+
+            return Task.FromResult( response );
+        }
+
+        //Handler on the 'Client' side of the system
+        [Route( "/Message[@type='Response' and @action='HeartBeat']" )]
+        public static Task HeartBeatResponseHandler( HeartBeatResponseMessage request )
+        {
+            Console.WriteLine( $"Received Response: {request?.Result?.Status}" );
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ EP03a - SocketsProgramming/Shared/XmlMessageProtocol.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using System.Text;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+
+namespace Shared {
+
+    public class XmlMessageProtocol : Protocol<XDocument> {
+
+        protected override XDocument Decode( byte[ ] message ) {
+            var xmlData = Encoding.UTF8.GetString(message);
+            var xmlReader = XmlReader.Create(new StringReader(xmlData), new XmlReaderSettings { DtdProcessing = DtdProcessing.Ignore});
+            return XDocument.Load( xmlReader );
+        }
+
+        protected override byte[ ] EncodeBody<T>( T message ) {
+            var sb = new StringBuilder();
+            var sw = new StringWriter(sb);
+            var xs = new XmlSerializer(typeof(T));
+            xs.Serialize( sw, message );
+            return Encoding.UTF8.GetBytes( sb.ToString( ) );
+        }
+    }
+}
